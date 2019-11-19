5f43066a13112971d6fe54dcb7aec22d3a0a461e
fam <fam1923@gmail.com>
2019-11-19 23:53:26 +0800

    AAA

--- tests/ImageShots.test.js
+++ tests/ImageShots.test.js
@@ -1,7 +1,9 @@
+import path from 'path';
 import initStoryshots, { imageSnapshot } from '@storybook/addon-storyshots';
 import { mount } from 'enzyme';
 
 initStoryshots({
+    configPath: path.join(__dirname, '..', '.storybook'),
     suite: 'Image storyshots',
-    test: imageSnapshot({})
+    test: imageSnapshot({storybookUrl: 'http://localhost:6006'})
 });
